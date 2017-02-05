f3bbde500f60237b96cedb31b9f2efb95b5fcd2d
Nate Clark <nate@hired.com>
2017-02-04 22:18:51 -0800

    add license and formatting

--- devicetypes/heythisisnate/nodemcu-connected-contact-sensor.src/nodemcu-connected-contact-sensor.groovy
+++ devicetypes/heythisisnate/nodemcu-connected-contact-sensor.src/nodemcu-connected-contact-sensor.groovy
@@ -1,7 +1,26 @@
+/**
+ *  NodeMCU Cloud Connected Contact Sensor
+ *
+ *  Copyright 2017 Nate Clark | @heythisisnate
+ *  
+ *  A desccription of this project and documentaion can be found at:
+ *  https://github.com/heythisisnate/nodemcu-smartthings-sensors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 metadata {
   definition (name: "NodeMCU Connected Contact Sensor", namespace: "heythisisnate", author: "Nate") {
     capability "Contact Sensor"
-	capability "Sensor"
+    capability "Sensor"
     command "open"
     command "close"
   }
@@ -13,7 +32,7 @@ metadata {
     }
 
     main "contact"
-	details "contact"
+    details "contact"
   }
 }
 
--- smartapps/heythisisnate/cloud-sensor.src/cloud-sensor.groovy
+++ smartapps/heythisisnate/cloud-sensor.src/cloud-sensor.groovy
@@ -1,8 +1,27 @@
+/**
+ *  NodeMCU Cloud Sensor App
+ *
+ *  Copyright 2017 Nate Clark | @heythisisnate
+ *  
+ *  A desccription of this project and documentaion can be found at:
+ *  https://github.com/heythisisnate/nodemcu-smartthings-sensors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 definition(
   name: "Cloud Sensor",
   namespace: "heythisisnate",
   author: "Nate Clark",
-  description: "A generic webhook handler for internet connected contact or motion sensors",
+  description: "A webhook handler for internet connected contact or motion sensors",
   category: "Safety & Security",
   iconUrl: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/Cat-SafetyAndSecurity.png",
   iconX2Url: "https://s3.amazonaws.com/smartapp-icons/SafetyAndSecurity/Cat-SafetyAndSecurity@2x.png"
